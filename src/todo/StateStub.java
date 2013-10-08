c8a0571816de2f173379f90c6f93867405666660
Karan Kamath <karankamath@nus.edu.sg>
2013-10-09 00:07:37 +0800

    Added compiling class StateStub

--- src/todo/StateStub.java
+++ src/todo/StateStub.java
@@ -1,5 +1,33 @@
 package todo;
 
-public class StateStub {
+import java.util.ArrayList;
+import java.util.Calendar;
 
+public class StateStub {
+	protected State getStateStub() {
+		State stub = new State();
+		
+		ArrayList<String> tags =  new ArrayList<String>();
+		tags.add("priority");
+		tags.add("home");
+		FloatingTask task1 = new FloatingTask("do something important", tags);
+		stub.addTask(task1);
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_WEEK_IN_MONTH, Calendar.THURSDAY);
+		DeadlineTask task2 = new DeadlineTask("Do V0.1", tags, cal);
+		task2.addTag("Work");
+		task2.removeTag("Home");
+		stub.addTask(task2);
+		
+		cal.add(Calendar.DAY_OF_WEEK_IN_MONTH, Calendar.SATURDAY);
+		Calendar cal2 = Calendar.getInstance();
+		cal2.add(Calendar.DAY_OF_WEEK_IN_MONTH, Calendar.MONDAY);
+		TimedTask task3 = new TimedTask("Watch TV", tags, cal, cal2 );
+		task3.addTag("Leisure");
+		task3.removeTag("Work");
+		stub.addTask(task3);
+		
+		return stub;
+	}
 }
