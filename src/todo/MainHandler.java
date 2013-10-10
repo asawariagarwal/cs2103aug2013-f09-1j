74a9a5907ca06d6084be779a2c1475e0c3672ded
Karan Kamath <karankamath@nus.edu.sg>
2013-10-11 03:53:35 +0800

    Added Storage Exception

--- src/todo/MainHandler.java
+++ src/todo/MainHandler.java
@@ -4,18 +4,22 @@ import java.io.IOException;
 
 public class MainHandler {
 	public static void main(String[] args){
+		State initState = new State();
+		initState.setFeedback("Corrupted Previous State");
 		try {
-			State initState = StorageManager.readStore();
-			CommandHandler initCommandHandler = new CommandHandler(initState);
-			testUI initUI = new testUI(initCommandHandler);
-			initUI.main(null);
+			initState = StorageManager.readStore();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
+		} finally {
+			CommandHandler initCommandHandler = new CommandHandler(initState);
+			testUI initUI = new testUI(initCommandHandler);
+			initUI.main(null);
 		}
 		//CommandHandler initCommandHandler = new CommandHandler(initState);
-		State dummy = new State();
-		dummy.setFeedback("Nothing to display");
+		//State dummy = new State();
+		//dummy.setFeedback("Nothing to display");
 		//CommandHandler initCommandHandler = new CommandHandler(dummy);
 		//UserInterface initUI = new SimpleUI();
 		//testUI initUI = new testUI(initCommandHandler);
