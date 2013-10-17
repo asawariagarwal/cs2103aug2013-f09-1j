f8419bcfd7666a4495b370df8ec46945843d8b47
Jishnu Mohan <mohanjishnu@gmail.com>
2013-10-18 00:38:03 +0800

    Logging for JSONStorage added

--- src/todo/JSONStorage.java
+++ src/todo/JSONStorage.java
@@ -1,6 +1,7 @@
 package todo;
 
 import java.io.BufferedReader;
+import java.util.logging.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
@@ -20,17 +21,28 @@ public class JSONStorage {
 	
 	private static String _filename = "C:\\ToDo\\taskstore.txt";
 	
+	protected static Logger storageLogger = Logger.getLogger("JSONStorage");
+	
+	/**
+	 * @return
+	 * @throws IOException
+	 * @throws ParseException
+	 * @throws java.text.ParseException
+	 */
 	protected State readStore() throws IOException, ParseException, java.text.ParseException{
 		File storeFile = new File(_filename);
 		boolean isCreated = storeFile.isFile();
 		if (!isCreated){
+			storageLogger.log(Level.INFO, "creating new file");
 			storeFile.getParentFile().mkdirs();
 			storeFile.createNewFile();
 			return new State();
 		} else {
+			storageLogger.log(Level.INFO, "reading from existing file");
 			BufferedReader storeReader = new BufferedReader (new FileReader (storeFile));
 			String jsonStore = storeReader.readLine();
 			storeReader.close();
+			storageLogger.log(Level.INFO, "read done");
 			return parseStore(jsonStore);
 		}
 	}
@@ -46,16 +58,19 @@ public class JSONStorage {
 		
 		State savedState = new State();
 		
+		storageLogger.log(Level.FINE, "Parsing Floating"); 
 		for (Object floatObj : floatingArray){
 			FloatingTask floatingTask = getFloatingTask(floatObj);
 			savedState.addTask(floatingTask);
 		}
 		
+		storageLogger.log(Level.FINE, "Parsing Timed"); 
 		for (Object timedObj : timedArray){
 			TimedTask timedTask = getTimedTask(timedObj);
 			savedState.addTask(timedTask);
 		}
 		
+		storageLogger.log(Level.FINE, "Parsing Deadline"); 
 		for (Object deadlineObj : deadlineArray){
 			DeadlineTask deadlineTask = getDeadlineTask(deadlineObj);
 			savedState.addTask(deadlineTask);
@@ -63,16 +78,20 @@ public class JSONStorage {
 		
 		savedState.setFeedback("Welcome");
 		
+		storageLogger.log(Level.INFO, "Done Parsing");
 		return savedState;
 	}
 
 	@SuppressWarnings("unchecked")
 	private DeadlineTask getDeadlineTask(Object deadlineObj)
 			throws java.text.ParseException {
+		
+		storageLogger.log(Level.FINE, "Getting from JSON");
 		JSONObject deadlineTaskJSON = (JSONObject) deadlineObj;
 		String taskDescription = (String) deadlineTaskJSON.get("description");
 		ArrayList<String> tags = (ArrayList<String>) deadlineTaskJSON.get("tags");
 		String deadlineString = (String) deadlineTaskJSON.get("deadline");
+		storageLogger.log(Level.FINE, "Got from JSON");
 		
 		String deadlineFormat = " 'by' EEEEEEEEE',' dd MMMMMMMMM',' yyyy ";
 		SimpleDateFormat curFormater = new SimpleDateFormat(deadlineFormat);
@@ -82,18 +101,22 @@ public class JSONStorage {
 		deadlineCalendar.setTime(deadlineDate);
 		
 		DeadlineTask deadlineTask = new DeadlineTask(taskDescription, tags, deadlineCalendar);
+		
+		storageLogger.log(Level.FINE, "Deadline Task Created");
 		return deadlineTask;
 	}
 
 	@SuppressWarnings("unchecked")
 	private TimedTask getTimedTask(Object timedObj)
 			throws java.text.ParseException {
+		storageLogger.log(Level.FINER, "Getting from JSON");
 		JSONObject timedTaskJSON = (JSONObject) timedObj;
 		String taskDescription = (String) timedTaskJSON.get("description");
 		ArrayList<String> tags = (ArrayList<String>) timedTaskJSON.get("tags");
 		
 		String startString = (String) timedTaskJSON.get("from");
 		String endString = (String) timedTaskJSON.get("to");
+		storageLogger.log(Level.FINER, "Got from JSON");
 		
 		String timedTaskFormat = "hh:mm aa 'on' EEEEEEEEE ',' dd MMMMMMMMM, yyyy ";
 		SimpleDateFormat curFormater = new SimpleDateFormat(timedTaskFormat);
@@ -106,18 +129,26 @@ public class JSONStorage {
 		toCalendar.setTime(toDate);
 		
 		TimedTask timedTask = new TimedTask(taskDescription, tags, fromCalendar, toCalendar);
+		storageLogger.log(Level.FINER, "Timed Task Created");
 		return timedTask;
 	}
 
 	@SuppressWarnings("unchecked")
 	private FloatingTask getFloatingTask(Object floatObj) {
+		storageLogger.log(Level.FINER, "Getting from JSON");
 		JSONObject floatingTaskJSON = (JSONObject) floatObj;
 		String taskDescription = (String) floatingTaskJSON.get("description");
 		ArrayList<String> tags = (ArrayList<String>) floatingTaskJSON.get("tags");
+		storageLogger.log(Level.FINER, "Got from JSON");
 		FloatingTask floatingTask = new FloatingTask(taskDescription, tags);
+		storageLogger.log(Level.FINER, "Floating Task Created");
 		return floatingTask;
 	}
 	
+	/**
+	 * @param saveState
+	 * @throws IOException
+	 */
 	@SuppressWarnings("unchecked")
 	protected void writeStore(State saveState) throws IOException{
 		
@@ -127,18 +158,22 @@ public class JSONStorage {
 		JSONArray timedArray = new JSONArray();
 		JSONArray deadlineArray = new JSONArray();
 		
+		storageLogger.log(Level.FINE, "Adding Floating"); 
 		for(FloatingTask floatingTask: saveState.getFloatingTasks()){
 			floatingArray.add(getFloatingJSON(floatingTask));
 		}
 		
+		storageLogger.log(Level.FINE, "Adding Timed"); 
 		for(TimedTask timedTask: saveState.getTimedTasks()){
 			timedArray.add(getTimedJSON(timedTask));
 		}
 		
+		storageLogger.log(Level.FINE, "Adding Deadline"); 
 		for(DeadlineTask deadlineTask: saveState.getDeadlineTasks()){
 			deadlineArray.add(getDeadlineJSON(deadlineTask));
 		}
 		
+		storageLogger.log(Level.INFO, "Putting Task Arrays into JSON");
 		jsonStore.put("floating",floatingArray);
 		jsonStore.put("timed", timedArray);
 		jsonStore.put("deadline", deadlineArray);
@@ -147,6 +182,7 @@ public class JSONStorage {
 		
 		BufferedWriter fileWriter = new BufferedWriter(new FileWriter(saveFile));
 		
+		storageLogger.log(Level.INFO, "Writing to file");
 		fileWriter.write(jsonStore.toJSONString());
 		
 		fileWriter.close();
@@ -159,6 +195,7 @@ public class JSONStorage {
 		taskObject.put("description",task.getTaskDescription());
 		JSONArray tags = getTagsJSON(task.getTags());
 		taskObject.put("tags",tags);
+		storageLogger.log(Level.FINE, "Created Floating JSON");
 		return taskObject;
 	}
 	
@@ -177,7 +214,7 @@ public class JSONStorage {
 		
 		taskObject.put("from", startString);
 		taskObject.put("to", endString);
-		
+		storageLogger.log(Level.FINE, "Created Timed JSON");
 		return taskObject;
 		
 	}
@@ -195,7 +232,7 @@ public class JSONStorage {
 		String deadlineString = sdf.format(task.getDeadline().getTime());
 		
 		taskObject.put("deadline", deadlineString);
-		
+		storageLogger.log(Level.FINE, "Created Deadline JSON");
 		return taskObject;
 	}
 	
@@ -205,6 +242,7 @@ public class JSONStorage {
 		for (String tag : tagArray){
 			tagJSON.add(tag);
 		}
+		storageLogger.log(Level.FINE, "Added all tags");
 		return tagJSON;
 	}
 	
