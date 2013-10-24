19ed6d8f07c5a2909c9ac156d8a14bc530cb63ec
Jishnu Mohan <mohanjishnu@gmail.com>
2013-10-25 01:01:59 +0800

    Removed UML from repo

---
+++ src/todo/StorageTest.java
@@ -0,0 +1,24 @@
+package todo;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class StorageTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testReadStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteStore() {
+		fail("Not yet implemented");
+	}
+
+}
