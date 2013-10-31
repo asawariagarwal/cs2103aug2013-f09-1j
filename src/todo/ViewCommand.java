bc42d0279b9f65a35e1fd1fc071900bfa109d070
Eugene Lim <eugene_hhlim@hotmail.com>
2013-10-31 15:55:05 +0800

    Added view timed and view deadline. Refactored ViewCommand.

--- src/todo/ViewCommand.java
+++ src/todo/ViewCommand.java
@@ -2,6 +2,7 @@ package todo;
 
 import java.util.Calendar;
 import java.util.TreeSet;
+import java.util.logging.*;
 
 /**
  * Subclass to encapsulate view commands
@@ -10,10 +11,16 @@ import java.util.TreeSet;
  * 
  */
 public class ViewCommand extends Command {
+	public static int MODE_VIEW_ALL = 0;
+	public static int MODE_VIEW_FLOATING = 1;
+	public static int MODE_VIEW_TIMED = 2;
+	public static int MODE_VIEW_DEADLINE = 3;
+	public static int MODE_VIEW_DATE = 4;
+	public static int MODE_VIEW_TAG = 5;
+	
 	private Calendar date;
 	private String tag;
-	private boolean isOrderedByTags = false;
-	private boolean isFloating = false;
+	private int mode;
 	
 	/**
 	 * Empty constructor for ViewCommand
@@ -23,6 +30,7 @@ public class ViewCommand extends Command {
 	 */
 	ViewCommand() {
 		super(false);
+		mode = MODE_VIEW_ALL;
 	}
 	
 	/**
@@ -35,29 +43,9 @@ public class ViewCommand extends Command {
 	 * If not, it is a view all command
 	 * 
 	 */
-	ViewCommand(boolean floating) {
+	ViewCommand(int mode) {
 		this();
-		isFloating = floating;
-	}
-	
-	/**
-	 * Constructor for ViewCommand
-	 * 
-	 * @param floating
-	 * 			true if ViewCommand is a view floating command
-	 * 
-	 * @param orderByTag
-	 * 			true if ViewCommand is ordered by tags
-	 * 
-	 * Represents a view floating command, if floating is true
-	 * If not, it is a view all command
-	 * View is ordered by tags if orderByTag is true.
-	 * 
-	 */
-	ViewCommand(boolean floating, boolean orderByTag) {
-		this();
-		isFloating = floating;
-		isOrderedByTags = orderByTag;
+		this.mode = mode;
 	}
 	
 	/**
@@ -72,6 +60,7 @@ public class ViewCommand extends Command {
 	ViewCommand(Calendar date) {
 		this();
 		this.date = date;
+		this.mode = MODE_VIEW_DATE;
 	}
 	
 	/**
@@ -86,13 +75,15 @@ public class ViewCommand extends Command {
 	ViewCommand(String tag) {
 		this();
 		this.tag = tag;
+		this.mode = MODE_VIEW_TAG;
 	}
 
 	@Override
 	protected boolean isValid(State state) {
 		return (isViewAll() ||
 				isViewFloating() ||
-				isViewOrderedByTags() ||
+				isViewDeadline() ||
+				isViewTimed() ||
 				isViewDate() ||
 				isViewTag());
 	}
@@ -102,7 +93,7 @@ public class ViewCommand extends Command {
 	 * 
 	 */
 	private boolean isViewAll() {
-		return (date == null && tag == null && !isOrderedByTags && !isFloating);
+		return mode == MODE_VIEW_ALL;
 	}
 	
 	/**
@@ -110,15 +101,23 @@ public class ViewCommand extends Command {
 	 * 
 	 */
 	private boolean isViewFloating() {
-		return (date == null && tag == null && !isOrderedByTags && isFloating);
+		return mode == MODE_VIEW_FLOATING;
 	}
 	
 	/**
-	 * Checks if command is a "view ordered by tags" command
+	 * Checks if command is a "view deadline" command
 	 * 
 	 */
-	private boolean isViewOrderedByTags() {
-		return (date == null && tag == null && isOrderedByTags);
+	private boolean isViewDeadline() {
+		return mode == MODE_VIEW_DEADLINE;
+	}
+	
+	/**
+	 * Checks if command is a "view timed" command
+	 * 
+	 */
+	private boolean isViewTimed() {
+		return mode == MODE_VIEW_TIMED;
 	}
 	
 	/**
@@ -126,7 +125,7 @@ public class ViewCommand extends Command {
 	 * 
 	 */
 	private boolean isViewDate() {
-		return (date != null && tag == null && !isOrderedByTags && !isFloating);
+		return (mode == MODE_VIEW_DATE && date != null);
 	}
 	
 	/**
@@ -134,70 +133,148 @@ public class ViewCommand extends Command {
 	 * 
 	 */
 	private boolean isViewTag() {
-		return (date == null && tag != null && !isOrderedByTags && !isFloating);
+		return (mode == MODE_VIEW_TAG && tag != null);
 	}
 
 	@Override
-	protected State execute(State state) {
-		State s = new State(state);
+	protected State execute(State state) throws Exception {
+		assert(this.isValid(state));
 		if (isViewAll()) {
-			s.setFeedback("viewing all tasks");
-			return s;
+			logger.log(Level.INFO, "executing view all");
+			return executeViewAll(state);
+		} else if (isViewFloating()) {
+			logger.log(Level.INFO, "executing view floating");
+			return executeViewFloating(state);
+		} else if (isViewDeadline()) {
+			logger.log(Level.INFO, "executing view deadline");
+			return executeViewDeadline(state);
+		} else if (isViewTimed()) {
+			logger.log(Level.INFO, "executing view timed");
+			return executeViewTimed(state);
+		} else if (isViewDate()) {
+			logger.log(Level.INFO, "executing view date");
+			return executeViewDate(state);
+		} else if (isViewTag()) {
+			logger.log(Level.INFO, "executing view tag");
+			return executeViewTag(state);
+		} else {
+			logger.log(Level.WARNING, "error executing view");
+			throw new Exception();
 		}
-		if (isViewFloating()) {
-			s.getDeadlineTasks().clear();
-			s.getTimedTasks().clear();
-			s.setFeedback("viewing floating tasks");
-			return s;
+	}
+	
+	/**
+	 * Executes view all command
+	 * @param state
+	 * 			current state of program
+	 * @return state after executing command
+	 */
+	private State executeViewAll(State state) {
+		State s = new State(state);
+		s.setFeedback("viewing all tasks");
+		return s;
+	}
+	
+	/**
+	 * Executes view floating command
+	 * @param state
+	 * 			current state of program
+	 * @return state after executing command
+	 */
+	private State executeViewFloating(State state) {
+		State s = new State(state);
+		s.getDeadlineTasks().clear();
+		s.getTimedTasks().clear();
+		s.setFeedback("viewing floating tasks");
+		return s;
+	}
+	
+	
+	/**
+	 * Executes view deadline command
+	 * @param state
+	 * 			current state of program
+	 * @return state after executing command
+	 */
+	private State executeViewDeadline(State state) {
+		State s = new State(state);
+		s.getFloatingTasks().clear();
+		s.getTimedTasks().clear();
+		s.setFeedback("viewing deadline tasks");
+		return s;
+	}
+	
+	
+	/**
+	 * Executes view timed command
+	 * @param state
+	 * 			current state of program
+	 * @return state after executing command
+	 */
+	private State executeViewTimed(State state) {
+		State s = new State(state);
+		s.getDeadlineTasks().clear();
+		s.getFloatingTasks().clear();
+		s.setFeedback("viewing timed tasks");
+		return s;
+	}
+	
+	/**
+	 * Executes view date command
+	 * @param state
+	 * 			current state of program
+	 * @return state after executing command
+	 */
+	private State executeViewDate(State state) {
+		State s = new State();
+		int dd = date.get(Calendar.DATE);
+		int mm = date.get(Calendar.MONTH);
+		int yy = date.get(Calendar.YEAR);
+		TreeSet<DeadlineTask> deadline = state.getDeadlineTasks();
+		TreeSet<TimedTask> timed = state.getTimedTasks();
+		for (DeadlineTask cur: deadline) {
+			if (dd == cur.getDeadline().get(Calendar.DATE) &&
+				mm == cur.getDeadline().get(Calendar.MONTH) &&
+				yy == cur.getDeadline().get(Calendar.YEAR)) {
+				s.addTask(cur);
+			}
 		}
-		if (isViewOrderedByTags()) {
-			// TODO How to represent a view ordered by tags??
+		for (TimedTask cur : timed) {
+			if (date.compareTo(cur.getStartDate()) >= 0 && date.compareTo(cur.getEndDate()) <= 0) {
+				s.addTask(cur);
+			}
 		}
-		if (isViewDate()) {
-			s = new State();
-			int dd = date.get(Calendar.DATE);
-			int mm = date.get(Calendar.MONTH);
-			int yy = date.get(Calendar.YEAR);
-			TreeSet<DeadlineTask> deadline = state.getDeadlineTasks();
-			TreeSet<TimedTask> timed = state.getTimedTasks();
-			for (DeadlineTask cur: deadline) {
-				if (dd == cur.getDeadline().get(Calendar.DATE) &&
-					mm == cur.getDeadline().get(Calendar.MONTH) &&
-					yy == cur.getDeadline().get(Calendar.YEAR)) {
-					s.addTask(cur);
-				}
+		s.setFeedback("viewing date: " + String.valueOf(dd) + "/" + String.valueOf(mm+1) + "/" + String.valueOf(yy));
+		return s;
+	}
+	
+	/**
+	 * Executes view tag command
+	 * @param state
+	 * 			current state of program
+	 * @return state after executing command
+	 */
+	private State executeViewTag(State state) {
+		State s = new State();
+		TreeSet<DeadlineTask> deadline = state.getDeadlineTasks();
+		TreeSet<TimedTask> timed = state.getTimedTasks();
+		TreeSet<FloatingTask> floating = state.getFloatingTasks();
+		for (DeadlineTask cur: deadline) {
+			if (cur.hasTag(tag)) {
+				s.addTask(cur);
 			}
-			for (TimedTask cur : timed) {
-				if (date.compareTo(cur.getStartDate()) >= 0 && date.compareTo(cur.getEndDate()) <= 0) {
-					s.addTask(cur);
-				}
-			}
-			s.setFeedback("viewing tasks by date: " + String.valueOf(dd) + "/" + String.valueOf(mm) + "/" + String.valueOf(yy));
-			return s;
 		}
-		if (isViewTag()) {
-			s = new State();
-			TreeSet<DeadlineTask> deadline = state.getDeadlineTasks();
-			TreeSet<TimedTask> timed = state.getTimedTasks();
-			TreeSet<FloatingTask> floating = state.getFloatingTasks();
-			for (DeadlineTask cur: deadline) {
-				if (cur.hasTag(tag)) {
-					s.addTask(cur);
-				}
+		for (TimedTask cur: timed) {
+			if (cur.hasTag(tag)) {
+				s.addTask(cur);
 			}
-			for (TimedTask cur: timed) {
-				if (cur.hasTag(tag)) {
-					s.addTask(cur);
-				}
-			}
-			for (FloatingTask cur: floating) {
-				if (cur.hasTag(tag)) {
-					s.addTask(cur);
-				}
-			}
-			s.setFeedback("viewing tasks by tag: " + tag);
-			return s;
 		}
-		return null;
+		for (FloatingTask cur: floating) {
+			if (cur.hasTag(tag)) {
+				s.addTask(cur);
+			}
+		}
+		s.setFeedback("viewing tasks by tag: " + tag);
+		return s;
 	}
 }
