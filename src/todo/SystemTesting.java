1b25a559d7ace8f4a5fbcbbd406c71dfd8bbdef9
Jishnu Mohan <mohanjishnu@gmail.com>
2013-11-01 08:25:33 +0800

    Running SystemTests now do not overwrite storage

--- src/todo/SystemTesting.java
+++ src/todo/SystemTesting.java
@@ -2,30 +2,38 @@ package todo;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SystemTesting {
 	
 	CommandHandler handler;
+	State savedState;
+	JSONStorage storage;
 	
 @Before
 public void SetUp(){
 	handler = new CommandHandler();
+	storage = new JSONStorage();
+	savedState = handler.getCurrentState();
+	//Hack until delete is modified.
+	ClearCommand clear = new ClearCommand();
+	handler.handle(clear);
 }
 	//Test to check if adding of a deadline task works 
 	@Test
-	public void DeadlineTest() throws ParseException {
+	public void addDeadlineTest() throws ParseException {
 		
-		SetUp();
 		
-		//Checks whether the feedback send to the GUi is correct 
-		State displayState  = handler.handleInput("add complete V0.3 by 25/10/2013 #CS2103");
+		//Checks whether the feedback send to the GUI is correct 
+		State displayState  = handler.handleInput("add complete V0.3 by 05/10/2013 #CS2103");
 		String feedback = displayState.getFeedback();
 		assertEquals("added new deadline task: complete V0.3", feedback);
 		
@@ -33,15 +41,14 @@ public void SetUp(){
 		String taskDes = displayState.getDeadlineTasks().last().getTaskDescription();
 		assertEquals("complete V0.3",taskDes);
 		
-		SimpleDateFormat curFormater = new SimpleDateFormat("dd/MM/yyyy");
-		Date dateObj = curFormater.parse("25/10/2013");
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(dateObj);
+		SimpleDateFormat curFormater = new SimpleDateFormat("MM/dd/yyyy");
+		Date dateObj = curFormater.parse("05/10/2013");
+		Calendar deadlineExpected = Calendar.getInstance();
+		deadlineExpected.setTime(dateObj);
 		
 		//Checks if the calendar object stored in the state is correct
-		String deadlineActual = displayState.getDeadlineTasks().last().getDeadline().toString();
-		String deadlineExpected = calendar.toString();
-		assertEquals(deadlineExpected, deadlineActual);
+		Calendar deadlineActual = displayState.getDeadlineTasks().last().getDeadline();
+		assertEquals(deadlineExpected.get(Calendar.DAY_OF_YEAR), deadlineActual.get(Calendar.DAY_OF_YEAR));
 		
 		//Checks if the hashtags stored in the task object in the state are correct
 		String hashtag = displayState.getDeadlineTasks().last().getTags().get(0);
@@ -52,10 +59,10 @@ public void SetUp(){
 	
 	//Test to check if adding of a floating task works
 	@Test
-	public void FloatingTest(){
+	public void addFloatingTest(){
 		
 		//Checks whether the feedback send to the GUi is correct 
-		State displayState2  =handler.handleInput("add read a book #hobby #interest ");
+		State displayState2  = handler.handleInput("add read a book #hobby #interest ");
 		String feedback = displayState2.getFeedback();
 		assertEquals("added new floating task: read a book", feedback);
 		
@@ -75,7 +82,7 @@ public void SetUp(){
 	
 	//Test to check if adding of a timed task works
 	@Test 
-	public void TimedTest() throws ParseException{
+	public void addTimedTest() throws ParseException{
 		
 		//Checks whether the feedback send to the GUi is correct 
 		State displayState3  =handler.handleInput("add attend seminar from 2/12/2013 to 5/12/2013 #work");
@@ -86,24 +93,22 @@ public void SetUp(){
 		String taskDes = displayState3.getTimedTasks().last().getTaskDescription();
 		assertEquals("attend seminar",taskDes);
 		
-		SimpleDateFormat curFormater = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat curFormater = new SimpleDateFormat("MM/dd/yyyy");
 		Date dateObj = curFormater.parse("2/12/2013");
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(dateObj);
+		Calendar startExpected = Calendar.getInstance();
+		startExpected.setTime(dateObj);
 		
 		
 		Date dateObj2 = curFormater.parse("5/12/2013");
-		Calendar calendar2 = Calendar.getInstance();
-		calendar2.setTime(dateObj2);
+		Calendar endExpected = Calendar.getInstance();
+		endExpected.setTime(dateObj2);
 		
 		//Checks if the calendar object stored in the state is correct
-		String startActual = displayState3.getTimedTasks().last().getStartDate().toString();
-		String startExpected = calendar.toString();
-		assertEquals(startExpected, startActual);
+		Calendar startActual = displayState3.getTimedTasks().last().getStartDate();
+		assertEquals(startExpected.get(Calendar.DAY_OF_YEAR), startActual.get(Calendar.DAY_OF_YEAR));
 		
-		String endActual = displayState3.getTimedTasks().last().getEndDate().toString();
-		String endExpected = calendar2.toString();
-		assertEquals(endExpected, endActual);
+		Calendar endActual = displayState3.getTimedTasks().last().getEndDate();
+		assertEquals(endExpected.get(Calendar.DAY_OF_YEAR), endActual.get(Calendar.DAY_OF_YEAR));
 		
 		//Checks if the hashtags stored in the task object in the state are correct
 		String hashtag = displayState3.getTimedTasks().last().getTags().get(0);
@@ -113,12 +118,20 @@ public void SetUp(){
 	
 	//Test to check if the feedback sent to the user for invalid command is correct
 	@Test
-	public void InvalidTask(){
+	public void deleteInvalidTask(){
 		
-		State displayState3  =handler.handleInput("bagweuigfwebuigcigcuw");
+		State displayState3  = handler.handleInput("delete nosuchtask");
 		String feedback = displayState3.getFeedback();
-		assertEquals("Invalid command", feedback);
+		assertEquals("no tasks containing nosuchtask can be found", feedback);
 		
 	}
 	
+	@After
+	public void restoreState(){
+		try {
+			storage.writeStore(savedState);
+		} catch (IOException e) {
+			fail("Write failed");
+		}
+	}
 }
