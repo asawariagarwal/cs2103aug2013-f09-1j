2b524247add908da1e1a7d90d89ce9b77e85cf5a
Eugene Lim <eugene_hhlim@hotmail.com>
2013-11-08 06:01:52 +0800

    Simple powersearch

--- src/todo/SearchCommand.java
+++ src/todo/SearchCommand.java
@@ -10,8 +10,8 @@ import java.util.logging.*;
  * 
  */
 public class SearchCommand extends Command {
-	private static final String FEEDBACK_NONE = "no tasks found for keyword: %1$s";
-	private static final String FEEDBACK_FOUND = "tasks found for keyword: %1$s";
+	private static final String FEEDBACK_NONE = "no tasks found for: %1$s";
+	private static final String FEEDBACK_FOUND = "tasks found for: %1$s";
 	
 	private static final String LOG_MESSAGE = "executing search";
 	private static final String LOG_NONE = "no tasks found";
@@ -40,8 +40,81 @@ public class SearchCommand extends Command {
 		assert(this.isValid());
 		logger.log(Level.INFO, LOG_MESSAGE);
 		
+		return executePowerSearch(state);
+		/*
 		State s = new State();
 		ArrayList<Task> found = state.getTasks(keyword);
+		if (found.isEmpty()) {
+			logger.log(Level.INFO, LOG_NONE);
+			s.setFeedback(new Feedback(String.format(FEEDBACK_NONE, keyword),false));
+			return s;
+		} else {
+			logger.log(Level.INFO, LOG_FOUND);
+			for (Task t: found) {
+				s.addTask(t);
+			}
+			s.setFeedback(new Feedback(String.format(FEEDBACK_FOUND, keyword),true));
+			return s;
+		}
+		*/
+	}
+	
+	//hackish powersearch
+	private State executePowerSearch(State state) throws Exception {
+		String[] keywords = keyword.split(" +");
+		ArrayList<Task> found = state.getAllTasks();
+		for (int i=0; i<keywords.length; i++) { 
+			if (keywords[i].charAt(0) == '#' && keywords[i].length() > 1) {
+				String tag = keywords[i].substring(1);
+				int j = 0;
+				while (j < found.size()) {
+					Task t = found.get(j);
+					if (!t.hasTag(tag)) {
+						found.remove(j);
+					} else {
+						j++;
+					}
+				}
+			} else if (keywords[i].charAt(0) == '-' && keywords[i].length() > 1) {
+				if (keywords[i].charAt(1) == '#') {
+					if (keywords[i].length() > 2) {
+						String tag = keywords[i].substring(2);
+						int j = 0;
+						while (j < found.size()) {
+							Task t = found.get(j);
+							if (t.hasTag(tag)) {
+								found.remove(j);
+							} else {
+								j++;
+							}
+						}
+					}
+				} else {
+					String word = keywords[i].substring(1);
+					int j = 0;
+					while (j < found.size()) {
+						Task t = found.get(j);
+						if (t.getTaskDescription().indexOf(word) != -1) {
+							found.remove(j);
+						} else {
+							j++;
+						}
+					}
+				}
+			} else {
+				String word = keywords[i].substring(1);
+				int j = 0;
+				while (j < found.size()) {
+					Task t = found.get(j);
+					if (t.getTaskDescription().indexOf(word) == -1) {
+						found.remove(j);
+					} else {
+						j++;
+					}
+				}
+			}
+		}
+		State s = new State();
 		if (found.isEmpty()) {
 			logger.log(Level.INFO, LOG_NONE);
 			s.setFeedback(new Feedback(String.format(FEEDBACK_NONE, keyword),false));
