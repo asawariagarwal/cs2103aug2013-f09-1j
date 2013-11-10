80d8549851ab80971357520065d57595942f8d2e
Karan Kamath <karankamath@nus.edu.sg>
2013-11-10 21:00:03 +0800

    Added javadoc for GUIFactory

--- src/todo/GUIFactory.java
+++ src/todo/GUIFactory.java
@@ -1,7 +1,18 @@
 package todo;
 
+/**
+ * Class to return a static instance of the GUI class
+ * 
+ * @author Karan
+ *
+ */
 public class GUIFactory {
 	static GUI _currentGUI;
+	/**
+	 * Routine to return a static instance of a GUI class
+	 * 
+	 * @return a static instance of a GUI class
+	 */
 	public static GUI getInstance() {
 		if(_currentGUI == null) {
 			return new GUI();
