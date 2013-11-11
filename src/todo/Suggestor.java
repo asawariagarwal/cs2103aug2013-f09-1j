e431fc85e741926a2a9a66996f9adc4a40bf74fa
Jishnu Mohan <mohanjishnu@gmail.com>
2013-11-11 22:07:26 +0800

    Javadoc comments for Suggestor

--- src/todo/Suggestor.java
+++ src/todo/Suggestor.java
@@ -12,7 +12,14 @@ import java.util.HashMap;
  */
 public class Suggestor {
 	
+	/**
+	 * Helpful string constants
+	 */
 	private static final String HASHTAG = "#";
+	
+	/**
+	 * Command syntax string constants
+	 */
 	private static final String ADD = "add";
 	private static final String VIEW = "view";
 	private static final String DELETE = "delete";
@@ -29,6 +36,9 @@ public class Suggestor {
 	private static final String TAG = "tag";
 	private static final String UNTAG = "untag";
 	
+	/**
+	 * View argument string constants
+	 */
 	private static final String NEXTDAY_LABEL = "tomorrow";
 	private static final String TODAY_LABEL = "today";
 	private static final String UNMARKED_LABEL = "pending";
@@ -40,31 +50,68 @@ public class Suggestor {
 	private static final String ALL_LABEL = "all";
 	private static final String FROM_LABEL = "from";
 	
-	private static ArrayList<String> commandList = new ArrayList<String>();	
+	/**
+	 * List to store all possible commands
+	 */
+	private static ArrayList<String> _commandList = new ArrayList<String>();	
 	
-	private State currentState;
+	/**
+	 * Variable to store current state
+	 */
+	private State _currentState;
 	
-	private HashMap<String,ArrayList<String>> commandSuggestionMap =
+	/**
+	 * Hashmap from command to possible suggestions
+	 */
+	private HashMap<String,ArrayList<String>> _commandSuggestionMap =
 			new HashMap<String,ArrayList<String>>();
 	
 	
+	/**
+	 * Constructors that loads constant suggestions
+	 */
 	public Suggestor(){
-		commandList.add(ADD);
-		commandList.add(DELETE);
-		commandList.add(VIEW);
-		commandList.add(CHANGE);
-		commandList.add(RESCHEDULE);
-		commandList.add(SEARCH);
-		commandList.add(UNDO);
-		commandList.add(REDO);
-		commandList.add(HELP);
-		commandList.add(CLEAR);
-		commandList.add(EXIT);
-		commandList.add(CLEAR);
-		commandList.add(MARK);
+		
+		loadCommandList();
 		
 		ArrayList<String> typeSuggestions = new ArrayList<String>();
 		ArrayList<String> viewSuggestions = new ArrayList<String>();
+		
+		loadTaskTypeSuggestions(typeSuggestions);
+		
+		_commandSuggestionMap.put(CLEAR, typeSuggestions);
+		
+		viewSuggestions.addAll(typeSuggestions);
+		addViewSpecificSuggestions(viewSuggestions);
+		
+		_commandSuggestionMap.put(VIEW, viewSuggestions);		
+	}
+
+	/**
+	 * Method to load constant commands to commandList
+	 */
+	private void loadCommandList() {
+		_commandList.add(ADD);
+		_commandList.add(DELETE);
+		_commandList.add(VIEW);
+		_commandList.add(CHANGE);
+		_commandList.add(RESCHEDULE);
+		_commandList.add(SEARCH);
+		_commandList.add(UNDO);
+		_commandList.add(REDO);
+		_commandList.add(HELP);
+		_commandList.add(CLEAR);
+		_commandList.add(EXIT);
+		_commandList.add(CLEAR);
+		_commandList.add(MARK);
+	}
+	
+	/**
+	 * Method to load suggestions based on task types
+	 * 
+	 * @param typeSuggestions contains the arrayList to be updated
+	 */
+	private void loadTaskTypeSuggestions(ArrayList<String> typeSuggestions) {
 		typeSuggestions.add(ALL_LABEL);
 		typeSuggestions.add(FLOATING_LABEL);
 		typeSuggestions.add(TIMED_LABEL);
@@ -72,17 +119,26 @@ public class Suggestor {
 		typeSuggestions.add(MARKED_LABEL);
 		typeSuggestions.add(EXPIRED_LABEL);
 		typeSuggestions.add(UNMARKED_LABEL);
-		
-		commandSuggestionMap.put(CLEAR, typeSuggestions);
-		
-		viewSuggestions.addAll(typeSuggestions);
+	}
+	
+	/**
+	 * Method to load suggestions specific to view
+	 * 
+	 * @param viewSuggestions contains the arrayList to be updated
+	 */
+	private void addViewSpecificSuggestions(ArrayList<String> viewSuggestions) {
 		viewSuggestions.add(TODAY_LABEL);
 		viewSuggestions.add(NEXTDAY_LABEL);
 		viewSuggestions.add(FROM_LABEL);
-		
-		commandSuggestionMap.put(VIEW, viewSuggestions);		
 	}
 	
+	
+	/**
+	 * Method to generate a suggestion given a certain input
+	 * 
+	 * @param inputString specifies input entered by user
+	 * @return the complete suggested String
+	 */
 	protected String getSuggestion(String inputString){
 		if (inputString.contains(" ")){
 			return getArgument(inputString);
@@ -91,13 +147,20 @@ public class Suggestor {
 		}
 	}
 	
+	/**
+	 * Method to complete user input if input is
+	 * substring of a command keyword
+	 * 
+	 * @param inputString specifies user input
+	 * @return completed command keyword
+	 */
 	private String getCommand(String inputString){
 		int inputLength = inputString.length();	
 		
 		if (inputString.isEmpty()){
 			return ADD;
 		} else {
-			for (String command : commandList){
+			for (String command : _commandList){
 				if (command.length()>=inputLength){
 					if (command.substring(0, inputLength)
 							.equalsIgnoreCase(inputString)){
@@ -109,6 +172,14 @@ public class Suggestor {
 		}
 	}
 	
+	/**
+	 * Method to return suggestions of
+	 * command argument based on which
+	 * command it is
+	 * 
+	 * @param inputString specifies input by user
+	 * @return completed suggestion
+	 */
 	private String getArgument(String inputString){
 		int indexOfSpace = inputString.indexOf(' ');
 		
@@ -117,8 +188,8 @@ public class Suggestor {
 		String argSnippet = inputString.substring(indexOfSpace+1);
 		int snipLength = argSnippet.length();
 		
-		if (commandSuggestionMap.containsKey(command.toLowerCase())){
-			ArrayList<String> argSuggestions = commandSuggestionMap.get(command);
+		if (_commandSuggestionMap.containsKey(command.toLowerCase())){
+			ArrayList<String> argSuggestions = _commandSuggestionMap.get(command);
 			for (String suggestion : argSuggestions){
 				if (suggestion.length()>=snipLength){
 					if (suggestion.substring(0, snipLength).equalsIgnoreCase(argSnippet)){
@@ -133,10 +204,16 @@ public class Suggestor {
 		
 	}
 	
+	/**
+	 * Method to update state information
+	 * for generation of dynamic (state-based) suggestions
+	 * 
+	 * @param newState specifies the latest state
+	 */
 	protected void updateState(State newState){
-		currentState = newState;
+		_currentState = newState;
 		
-		ArrayList<Task> taskList = currentState.getAllTasks();
+		ArrayList<Task> taskList = _currentState.getAllTasks();
 		ArrayList<String> descriptionList = new ArrayList<String>();
 		ArrayList<String> hashtagList = new ArrayList<String>();
 		ArrayList<String> completedList = new ArrayList<String>();
@@ -154,18 +231,31 @@ public class Suggestor {
 			}
 		}
 		
-		commandSuggestionMap.put(DELETE, descriptionList);
-		commandSuggestionMap.put(CHANGE, descriptionList);
-		commandSuggestionMap.put(RESCHEDULE, descriptionList);
-		commandSuggestionMap.put(TAG, descriptionList);
-		commandSuggestionMap.put(UNTAG, descriptionList);
-		commandSuggestionMap.put(MARK,pendingList);
-		commandSuggestionMap.put(UNMARK,completedList);
+		updateMapping(descriptionList, hashtagList, completedList, pendingList);	
+	}
+
+	/**
+	 * Updates HashMap based on State information
+	 * 
+	 * @param descriptionList contains a List of Task Descriptions
+	 * @param hashtagList contains a List of Hashtags used
+	 * @param completedList contains a List of descriptions of completed tasks
+	 * @param pendingList contains a List of descriptions of pending tasks
+	 */
+	private void updateMapping(ArrayList<String> descriptionList,
+			ArrayList<String> hashtagList, ArrayList<String> completedList,
+			ArrayList<String> pendingList) {
+		_commandSuggestionMap.put(DELETE, descriptionList);
+		_commandSuggestionMap.put(CHANGE, descriptionList);
+		_commandSuggestionMap.put(RESCHEDULE, descriptionList);
+		_commandSuggestionMap.put(TAG, descriptionList);
+		_commandSuggestionMap.put(UNTAG, descriptionList);
+		_commandSuggestionMap.put(MARK,pendingList);
+		_commandSuggestionMap.put(UNMARK,completedList);
 		
-		ArrayList<String> standardViewSugg = commandSuggestionMap.get(VIEW);
+		ArrayList<String> standardViewSugg = _commandSuggestionMap.get(VIEW);
 		standardViewSugg.addAll(hashtagList);
-		commandSuggestionMap.put(VIEW,standardViewSugg);
-		
+		_commandSuggestionMap.put(VIEW,standardViewSugg);
 	}
 	
 	
